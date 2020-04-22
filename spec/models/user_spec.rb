d2af78c4a23216478ca1ad316802fba26b9189b8
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-04-22 17:30:24 +0200

    User model and its silly tests.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before(:each) { @user = User.new(email: 'user@example.com') }
+
+  subject { @user }
+
+  it { should be_valid }
+
+  it { should respond_to(:email) }
+
+  it '#email returns a string' do
+    expect(@user.email).to match 'user@example.com'
+  end
+
+  describe 'when email is blank' do
+    before { @user.email = ' ' }
+    it { should_not be_valid }
+  end
+
+  it '.all returns a list of users' do
+    @user.save
+    expect(User.all.count).not_to eq 0
+  end
+end
